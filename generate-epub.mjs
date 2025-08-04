cec901eeb28e2101f9ab2907dbca421b62e8d6bc
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-08-04 16:38:30 +0300

    Update generate-epub.mjs

--- generate-epub.mjs
+++ generate-epub.mjs
@@ -14,7 +14,7 @@ import path    from 'path';
 function html(title, body) {
   return `<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml XHTML namespace XHTML namespace  ">
+xmlns="http://www.w3.org/1999/xhtml"
 <head>
   <title>${title}</title>
   <meta charset="utf-8"/>
@@ -81,25 +81,69 @@ mapped.forEach(ch => {
 
 // content.opf
 const opf = `<?xml version="1.0" encoding="utf-8"?>
-<package xmlns="http://www.idpf.org/2007/opf OPF Namespace OPF Namespace  " version="3.0" xml:lang="en" unique-identifier="uid">
+<package xmlns="http://www.idpf.org/2007/opf" version="3.0" xml:lang="en" unique-identifier="uid">
   <metadata xmlns:dc="http://purl.org/dc/elements/1.1/   ">
     <dc:identifier id="uid">urn:uuid:${crypto.randomUUID()}</dc:identifier>
     <dc:title>${xmlEscape(process.env.BOOK_TITLE)}</dc:title>
     <dc:creator>${xmlEscape(process.env.BOOK_AUTHOR)}</dc:creator>
     <dc:description>${xmlEscape(process.env.BOOK_DESC)}</dc:description>
     <dc:language>en</dc:language>
-        ${coverMime ? '<meta name="cover" content="cover"/>' : ''}
-    <meta property="dcterms:modified">${new Date().toISOString().replace(/\.\d{3}Z$/, 'Z')}</meta>
+${coverMime ? '<meta name="cover" content="cover-img"/>' : ''}
+<meta property="dcterms:modified">${new Date().toISOString().replace(/\.\d{3}Z$/, 'Z')}</meta>
   </metadata>
   <manifest>
+${coverMime ? `<item id="cover-img" href="cover.jpg" media-type="${coverMime}" properties="cover-image"/>` : ''}
 ${mapped.map(ch => `    <item id="${ch.id}" href="${ch.file}" media-type="application/xhtml+xml"/>`).join('\n')}
-${coverMime ? `    <item id="cover" href="cover.jpg" media-type="${coverMime}"/>` : ''}
+ <item id="toc"  href="toc.xhtml"  media-type="application/xhtml+xml" properties="nav"/>
+ <item id="ncx"  href="toc.ncx"    media-type="application/x-dtbncx+xml"/>
   </manifest>
-  <spine>
+  <spine toc="ncx">
 ${mapped.map(ch => `    <itemref idref="${ch.id}"/>`).join('\n')}
   </spine>
 </package>`;
 OEBPS.file('content.opf', opf);
+// Navigation Document (OEBPS/toc.xhtml)
+function navToc(chapters) {
+  const lis = chapters.map((c, idx) =>
+    `<li><a href="${c.file}">${xmlEscape(c.title)}</a></li>`).join('\n');
+
+  return `<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+<head>
+  <title>Table of Contents</title>
+</head>
+<body>
+  <nav epub:type="toc" id="toc">
+    <h1>Table of Contents</h1>
+    <ol>${lis}</ol>
+  </nav>
+</body>
+</html>`;
+}
+OEBPS.file('toc.xhtml', navToc(mapped));
+// legacy noc
+function ncxToc(chapters) {
+  const navPoints = chapters.map((c, idx) => `
+  <navPoint id="np-${c.id}" playOrder="${idx + 1}">
+    <navLabel><text>${xmlEscape(c.title)}</text></navLabel>
+    <content src="${c.file}"/>
+  </navPoint>`).join('\n');
+
+  return `<?xml version="1.0" encoding="utf-8"?>
+<ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
+  <head>
+    <meta name="dtb:uid" content="urn:uuid:${crypto.randomUUID()}"/>
+    <meta name="dtb:depth" content="1"/>
+    <meta name="dtb:totalPageCount" content="0"/>
+    <meta name="dtb:maxPageNumber" content="0"/>
+  </head>
+  <docTitle><text>${xmlEscape(process.env.BOOK_TITLE)}</text></docTitle>
+  <navMap>${navPoints}</navMap>
+</ncx>`;
+}
+OEBPS.file('toc.ncx', ncxToc(mapped));
+
 
 // ---------- write file ----------
 const safeName = process.env.OUTPUT_FILENAME.replace(/\s+/g, '-');
